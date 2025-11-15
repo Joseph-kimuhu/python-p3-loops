3e747e0a04d734d525796d11ef9025390b47c4bb
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-11-15 09:32:56 +0300

    first commit

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
